dec79e8b1faabaffc28a20d718ee5c8bb88c6270
Andy H. Jung <commaniakr@gmail.com>
2020-07-12 15:55:36 +0900

    feat: 지하철역 CRD 구현 완료.

--- src/main/java/wooteco/subway/admin/controller/StationController.java
+++ src/main/java/wooteco/subway/admin/controller/StationController.java
@@ -4,12 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.StationCreateRequest;
@@ -24,6 +19,7 @@ public class StationController {
 		this.stationService = stationService;
 	}
 
+	@CrossOrigin(origins="http://localhost:8090")
 	@PostMapping("/stations")
 	public ResponseEntity<StationResponse> createStation(@RequestBody StationCreateRequest view) {
 		Station station = stationService.save(view.toStation());
@@ -33,12 +29,14 @@ public class StationController {
 			.body(StationResponse.of(station));
 	}
 
+	@CrossOrigin(origins="http://localhost:8090")
 	@GetMapping("/stations")
 	public ResponseEntity<List<StationResponse>> showStations() {
 		return ResponseEntity.ok()
 			.body(StationResponse.listOf(stationService.findAll()));
 	}
 
+	@CrossOrigin(origins="http://localhost:8090")
 	@DeleteMapping("/stations/{id}")
 	public ResponseEntity<StationResponse> deleteStation(@PathVariable Long id) {
 		stationService.delete(id);
